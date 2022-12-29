404c76d4d1c478d9fc792049eeb85c8bc924ec16
Dr. Seek <75817900+dreamy32@users.noreply.github.com>
2022-12-29 14:58:55 -0500

    new tileset I need json

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@
 import { AnimatedSprite, Application, Assets, SCALE_MODES, settings, Sprite, Spritesheet, Texture } from 'pixi.js';
 import { Map } from "./Map";
 import { Animations, Player } from "./Entity";
-import { generateTextures} from "./functions";
+import { generateTextures } from "./functions";
 import { InputReader, inputTypes, KeysPressed } from './InputReader';
 import { Vect2D } from './Vect2D';
 
@@ -18,18 +18,18 @@ const app = new Application({
   view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
   resolution: window.devicePixelRatio || 1,
   autoDensity: true,
-  width: STAGE_WIDTH, 
+  width: STAGE_WIDTH,
   height: STAGE_HEIGHT,
   backgroundColor: 0xAAAAAA,
   antialias: false,
+
 });
 
 InputReader.initialize();
-
 settings.SCALE_MODE = SCALE_MODES.NEAREST;
+settings.WRAP_MODE
 
-
-await Assets.init({manifest: "manifest.json"});
+await Assets.init({ manifest: "manifest.json" });
 const mapAssets = await Assets.loadBundle("maps");
 const fontAssets = await Assets.loadBundle("fonts");
 const textureAssets = await Assets.loadBundle("textures");
@@ -49,12 +49,12 @@ app.ticker.add(delta => updateLoop(delta));
 
 const dazelArray: Array<Texture> = generateTextures(textureAssets.dazel, TILE_RESOLUTION, 9, 9);
 
-const dazelAnimation : Animations = {
-  frontWalk : dazelArray.slice(0, 4),
-  frontAttack : dazelArray.slice(4, 8)
+const dazelAnimation: Animations = {
+  frontWalk: dazelArray.slice(0, 4),
+  frontAttack: dazelArray.slice(4, 8)
 }
 
-const dazel = new Player(dazelAnimation, new Vect2D(30,30))
+const dazel = new Player(dazelAnimation, new Vect2D(30, 30))
 dazel.init(app);
 
 function updateLoop(_: number) {
